9822153217f43dde1f0d588fbdcf5fe95b8d7884
lean-apple <78718413+lean-apple@users.noreply.github.com>
2024-09-19 23:12:28 +0200

    feat: improve design

--- backend/assets/tailwind.config.js
+++ backend/assets/tailwind.config.js
@@ -23,6 +23,15 @@ module.exports = {
     extend: {
       colors: {
         brand: "#FD4F00",
+      },
+      keyframes: {
+        'spin-slow': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      },
+      animation: {
+        'spin-slow': 'spin-slow 10s linear infinite',
       }
     },
   },
--- backend/lib/dotferno_web/live/demo.live.ex
+++ backend/lib/dotferno_web/live/demo.live.ex
@@ -15,7 +15,13 @@ defmodule DotfernoWeb.DemoLive do
 
     socket = update_state(%{buckets_today: buckets_today}, socket)
     socket = update_state(%{buckets_year: buckets_year}, socket)
-    socket = assign(socket, biggest_today: Aggregator.biggest_today(), biggest_week: Aggregator.biggest_week())
+
+    socket =
+      assign(socket,
+        biggest_today: Aggregator.biggest_today(),
+        biggest_week: Aggregator.biggest_week()
+      )
+
     socket = assign(socket, dark_mode_hook: "dark-mode")
 
     PubSub.subscribe(Dotferno.PubSub, "buckets")
@@ -25,21 +31,35 @@ defmodule DotfernoWeb.DemoLive do
   end
 
   @impl true
-  def handle_info(%{buckets_today: _}=e, socket) do
+  def handle_info(%{buckets_today: _} = e, socket) do
     socket = update_state(e, socket)
-    send_update(DotfernoWeb.Components.ChartComponent, id: :chart_today, event: "update_chart", y: socket.assigns.buckets_today, x: socket.assigns.timings_today)
+
+    send_update(DotfernoWeb.Components.ChartComponent,
+      id: :chart_today,
+      event: "update_chart",
+      y: socket.assigns.buckets_today,
+      x: socket.assigns.timings_today
+    )
+
     {:noreply, socket}
   end
 
   @impl true
-  def handle_info(%{buckets_year: _}=e, socket) do
+  def handle_info(%{buckets_year: _} = e, socket) do
     socket = update_state(e, socket)
-    send_update(DotfernoWeb.Components.ChartComponent, id: :chart_year, event: "update_chart", y: socket.assigns.buckets_year, x: socket.assigns.timings_year)
+
+    send_update(DotfernoWeb.Components.ChartComponent,
+      id: :chart_year,
+      event: "update_chart",
+      y: socket.assigns.buckets_year,
+      x: socket.assigns.timings_year
+    )
+
     {:noreply, socket}
   end
 
   @impl true
-  def handle_info(%{biggest_today: _, biggest_week: _}=e, socket) do
+  def handle_info(%{biggest_today: _, biggest_week: _} = e, socket) do
     {:noreply, assign(socket, e)}
   end
 
@@ -51,26 +71,45 @@ defmodule DotfernoWeb.DemoLive do
     socket = update_state(%{buckets_today: buckets_today}, socket)
     socket = update_state(%{buckets_year: buckets_year}, socket)
 
-    send_update(DotfernoWeb.Components.ChartComponent, id: :chart_today, event: "update_chart", y: socket.assigns.buckets_today, x: socket.assigns.timings_today)
-    send_update(DotfernoWeb.Components.ChartComponent, id: :chart_year, event: "update_chart", y: socket.assigns.buckets_year, x: socket.assigns.timings_year)
+    send_update(DotfernoWeb.Components.ChartComponent,
+      id: :chart_today,
+      event: "update_chart",
+      y: socket.assigns.buckets_today,
+      x: socket.assigns.timings_today
+    )
+
+    send_update(DotfernoWeb.Components.ChartComponent,
+      id: :chart_year,
+      event: "update_chart",
+      y: socket.assigns.buckets_year,
+      x: socket.assigns.timings_year
+    )
 
     {:noreply, socket}
   end
 
   def update_state(%{buckets_today: {buckets_today, timings}}, socket) do
-    buckets_today = buckets_today |> Enum.map(fn x -> Dotferno.Format.plank_to_dot x end)
-    x_today = Enum.map(1..length(buckets_today), &(&1))
+    buckets_today = buckets_today |> Enum.map(fn x -> Dotferno.Format.plank_to_dot(x) end)
+    x_today = Enum.map(1..length(buckets_today), & &1)
     sum_today = Enum.sum(buckets_today) |> Kernel.round()
 
-    assign(socket, buckets_today: buckets_today, sum_today: sum_today, timings_today: render_timings(timings))
+    assign(socket,
+      buckets_today: buckets_today,
+      sum_today: sum_today,
+      timings_today: render_timings(timings)
+    )
   end
 
   def update_state(%{buckets_year: {buckets_year, timings}}, socket) do
-    buckets_year = buckets_year |> Enum.map(fn x -> Dotferno.Format.plank_to_dot x end)
-    x_year = Enum.map(1..length(buckets_year), &(&1))
+    buckets_year = buckets_year |> Enum.map(fn x -> Dotferno.Format.plank_to_dot(x) end)
+    x_year = Enum.map(1..length(buckets_year), & &1)
     sum_year = Enum.sum(buckets_year) |> Kernel.round()
 
-    assign(socket, buckets_year: buckets_year, sum_year: sum_year, timings_year: render_dates(timings))
+    assign(socket,
+      buckets_year: buckets_year,
+      sum_year: sum_year,
+      timings_year: render_dates(timings)
+    )
   end
 
   def render_timings(timings) do
@@ -94,7 +133,7 @@ defmodule DotfernoWeb.DemoLive do
     <div class="flex flex-col xs:flex-row justify-center md:justify-between w-full">
       <div class="flex justify-end">
         <div class="p-4">
-            <div class="flex rounded-lg items-center flex-col border glow pt-6 pb-6 pl-8 pr-8 bg-white dark:bg-gray-900 antialiased dark:border-gray-900">
+            <div class="flex font-semibold rounded-lg items-center flex-col border glow py-6 px-6 bg-white dark:bg-gray-900 antialiased dark:border-gray-900">
               <div class="text-xs">
                 24 hrs
               </div>
@@ -105,7 +144,7 @@ defmodule DotfernoWeb.DemoLive do
         </div>
 
         <div class="p-4">
-            <div class="flex rounded-lg items-center flex-col border glow pt-6 pb-6 pl-8 pr-8 bg-white dark:bg-gray-900 antialiased dark:border-gray-900">
+            <div class="flex font-semibold rounded-lg items-center flex-col border glow py-6 px-6 bg-white dark:bg-gray-900 antialiased dark:border-gray-900">
               <div class="text-xs">
                 7 days
               </div>
@@ -121,21 +160,21 @@ defmodule DotfernoWeb.DemoLive do
     <!-- card 1 -->
     <div class="p-4 w-full">
         <div class="flex rounded-lg h-full flex-col">
-            <div class="flex items-center mb-3">
-                <h2 class="text-lg font-medium text-center md:text-left  w-full">
-                  Burns per hour (24 hrs)
-                </h2>
-            </div>
-            <div>
+        <div class="flex items-center mb-5">
+        <h2 class="text-lg sm:text-xl md:text-2xl font-bold text-center md:text-left w-full text-gray-900 dark:text-gray-100 tracking-tight leading-tight border-b-2 border-pink-500 pb-2">
+        Burns per hour (24 hrs)
+        </h2>
+          </div>
+            <div class="text-gray-900 dark:text-white">
               <.live_component module={DotfernoWeb.Components.ChartComponent} id={:chart_today} y={buckets_today} x={timings_today} type="bar" />
             </div>
         </div>
     </div>
 
-<div class="flex flex-col xs:flex-row xs:justify-center md:justify-start w-full">
-  <div class="p-4 w-full xs:w-1/2 md:w-auto">
+    <div class="flex flex-col xs:flex-row xs:justify-center md:justify-start w-full">
+    <div class="p-4 w-full xs:w-1/2 md:w-auto">
       <div class="flex items-center mb-3">
-        <h2 class="text-lg font-medium text-center md:text-left w-full">
+        <h2 class="text-lg font-semibold text-center md:text-left w-full">
         Top today
         </h2>
       </div>
@@ -162,7 +201,7 @@ defmodule DotfernoWeb.DemoLive do
         </div>
         <div class="p-4 w-full xs:w-1/2 md:w-auto">
             <div class="flex items-center md:items-left mb-3">
-                <h2 class="text-lg font-medium text-center md:text-left w-full md:w-auto font-medium text-gray-900 dark:text-white">
+                <h2 class="text-lg font-semibold text-center md:text-left w-full md:w-auto text-gray-900 dark:text-white">
                   Top this week
                 </h2>
             </div>
@@ -191,14 +230,23 @@ defmodule DotfernoWeb.DemoLive do
 
     <div class="p-4 w-full">
       <div class="flex rounded-lg h-full flex-col">
-          <div class="flex items-center mb-3">
-              <h2 class="text-lg font-medium text-center md:text-left w-full">
-                Burns per day (7 days)
-              </h2>
-          </div>
-          <div class="text-white">
-            <.live_component module={DotfernoWeb.Components.ChartComponent} id={:chart_year} y={buckets_year} x={timings_year} type="bar" />
-          </div>
+        <div class="flex items-center mb-5">
+          <h2 class="text-lg sm:text-xl md:text-2xl font-bold text-center md:text-left w-full text-gray-900 dark:text-gray-100 tracking-tight leading-tight border-b-2 border-pink-500 pb-2">
+            Burns per day (7 days)
+          </h2>
+        </div>
+        <div class="text-gray-900 dark:text-white">
+        <.live_component module={DotfernoWeb.Components.ChartComponent}
+          id={:chart_year}
+          y={buckets_year}
+          x={timings_year}
+          type="bar"
+          class="text-gray-900 dark:text-white"
+          data-config="{ chart: {...} }"
+          data-series={Jason.encode!(buckets_year)}
+          data-categories={Jason.encode!(timings_year)}
+        />
+      </div>
       </div>
     </div>
     </div>
